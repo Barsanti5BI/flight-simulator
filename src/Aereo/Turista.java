4cbd7cb74b498f4e0a1acc80a065dbed115ef293
Perin <perlnmarck@gmail.com>
2023-12-02 09:12:06 +0100

    per te alessio

---
+++ src/Aereo/Turista.java
@@ -0,0 +1,14 @@
+package Aereo;
+
+public class Turista {
+    String nomeAereo;
+    String tipoCoda;
+    String Nome;
+    String Posto;
+    public Turista(String tipoCoda, String Nome, String Posto,String nomeAereo){
+        this.tipoCoda = tipoCoda;
+        this.Nome = Nome;
+        this.Posto = Posto;
+        this.nomeAereo = nomeAereo;
+    }
+}
