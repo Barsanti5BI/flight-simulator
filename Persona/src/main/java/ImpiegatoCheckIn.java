eb48d09e52ef548ba5a528c24a1ad8d486811226
thomas <53865372+thomasgarbui@users.noreply.github.com>
2024-01-04 16:06:06 +0100

    sistemazione

--- Persona/src/main/java/ImpiegatoCheckIn.java
+++ Persona/src/main/java/ImpiegatoCheckIn.java
@@ -1,3 +1,5 @@
+import java.security.SecureRandom;
+
 public class ImpiegatoCheckIn extends Persona{
 
     public ImpiegatoCheckIn(Documento doc){
@@ -6,10 +8,25 @@ public class ImpiegatoCheckIn extends Persona{
     public void run(){
 
     }
-    public String GeneraEtichetta(/*param*/){
-        String etichetta = "";
-        //to do
+    public String GeneraEtichetta(String AeroportoPartenza, String AeroportoArrivo){
+        String codUnivoco = "";
+        String etichetta = AeroportoPartenza +" "+ AeroportoArrivo + " CODICE UNIVOCO QUI";
+
         return etichetta;
+    }
+    public String generaCodiceUnivoco(int length) {
+
+        String caratteri = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+        SecureRandom random = new SecureRandom();
+        StringBuilder code = new StringBuilder();
+
+        for (int i = 0; i < length; i++) {
+            int randomIndex = random.nextInt(caratteri.length());
+            code.append(caratteri.charAt(randomIndex));
+        }
+
+        return code.toString();
     }
     public String GeneraCartaImbarco(/*param*/){
         String cartaImbarco = "";
