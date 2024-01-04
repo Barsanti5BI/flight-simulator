afa545c37d98ac3097c8297bf1e1dc4b2af994cf
ThomasCasonato <118836207+ThomasCasonato@users.noreply.github.com>
2024-01-04 16:51:26 +0100

    Manutentore

--- src/Persona/untitled/src/main/java/Manutentore.java
+++ src/Persona/untitled/src/main/java/Manutentore.java
@@ -3,6 +3,9 @@ package Persona.untitled.src.main.java;
 import javax.print.Doc;
 
 public class Manutentore extends Persona{
+    int livelloCarburante;
+    boolean inFunzione;
+    boolean stato;
     public Manutentore(Documento doc){
         super(doc);
     }
@@ -10,12 +13,24 @@ public class Manutentore extends Persona{
 
     }
     public void ControlloStatiApparecchiature(){
-
+    if(stato){
+        System.out.println("Sistemi OK");
+    } else{
+        System.out.println("Sistemi KO");
+    }
     }
     public void ControlloCarburante(){
-
+        if (livelloCarburante > 0) {
+            System.out.println("Il veicolo ha ancora carburante.");
+        } else {
+            System.out.println("Il livello di carburante è basso. Rifornire!");
+        }
     }
     public void ControlloTurbine(){
-
+        if (inFunzione) {
+            System.out.println("La turbina è in funzione.");
+        } else {
+            System.out.println("La turbina è spenta.");
+        }
     }
 }
