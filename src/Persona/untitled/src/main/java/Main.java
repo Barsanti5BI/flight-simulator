01947828bd4ebdfaad5f78beeb1ddd61958b42ae
willyyyyyy0000333 <squizz03@gmail.com>
2024-01-04 17:14:08 +0100

    Modifica classe Persona

--- src/Persona/untitled/src/main/java/Main.java
+++ src/Persona/untitled/src/main/java/Main.java
@@ -1,3 +1,5 @@
+package Persona.untitled.src.main.java;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
--- src/Persona/untitled/src/main/java/Persona.java
+++ src/Persona/untitled/src/main/java/Persona.java
@@ -5,11 +5,13 @@ public class Persona extends Thread{
     public Persona(Documento doc){
         this.doc = doc;
     }
+
     public void run(){
     }
     public void InserimentoCoda(Coda coda){
-
+        coda.push(this);
     }
     public void PopCoda(Coda coda){
+        coda.pop();
     }
 }
