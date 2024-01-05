8ddf38fdae343989b81748efdb431203e96d43a6
arloghiro <119437097+arloghiro@users.noreply.github.com>
2024-01-05 16:59:05 +0100

    Sistemazioni con nuove classi

--- src/Persona/Bagaglio.java
+++ src/Persona/Bagaglio.java
@@ -6,10 +6,10 @@ public class Bagaglio {
     private boolean daStiva;
     private int peso;
     private String misure;
-    private String etichetta;
+    private Etichetta etichetta;
     private List<String>oggettiContenuti;
 
-    public Bagaglio(boolean daStiva,int peso,String misure,String etichetta,List<String>oggettiContenuti){
+    public Bagaglio(boolean daStiva,int peso,String misure,Etichetta etichetta,List<String>oggettiContenuti){
         this.daStiva = daStiva;
         this.oggettiContenuti = oggettiContenuti;
         this.etichetta = etichetta;
@@ -24,6 +24,6 @@ public class Bagaglio {
         return peso;
     }
     public String getMisure(){ return misure;}
-    public String getEtichetta(){ return etichetta;}
+    public Etichetta getEtichetta(){ return etichetta;}
     public List<String> getOggettiContenuti(){return oggettiContenuti;}
 }
--- src/Persona/Turista.java
+++ src/Persona/Turista.java
@@ -2,10 +2,10 @@ package Persona;
 
 public class Turista extends Persona{
     private Bagaglio bag;
-    private String cartaImbarco;
+    private CartaImbarco cartaImbarco;
     private String destinazione;
 
-    public Turista(Documento doc,Bagaglio bag, String cartaImbarco) {
+    public Turista(Documento doc,Bagaglio bag, CartaImbarco cartaImbarco) {
         super(doc);
         this.bag = bag;
         this.cartaImbarco = cartaImbarco;
