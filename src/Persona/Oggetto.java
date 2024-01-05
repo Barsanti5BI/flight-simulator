0e755c9a986e7a984f2c11d87f98508eaa632aba
willyyyyyy0000333 <squizz03@gmail.com>
2024-01-05 17:17:13 +0100

    Aggiunta classe Oggetto

---
+++ src/Persona/Oggetto.java
@@ -0,0 +1,12 @@
+package Persona;
+
+public class Oggetto {
+    private String nome;
+
+    public Oggetto(String n){
+        this.nome = n;
+    }
+
+    //Metodo get
+    public String getNome(){return nome;}
+}
