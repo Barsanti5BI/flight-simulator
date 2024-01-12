dec632d9cf8ec60ef62d425fa9b8ddb13c88bb66
arloghiro <119437097+arloghiro@users.noreply.github.com>
2024-01-12 08:10:25 +0100

    Modifica cartaImbarco

--- src/Persona/CartaImbarco.java
+++ src/Persona/CartaImbarco.java
@@ -7,16 +7,17 @@ public class CartaImbarco {
     private String cognomePasseggero;
     private int gate;
     private LocalDate orario;
-    private String arrivo;
+    private String partenza;
     private String destinazione;
     private String idRiconoscimentoBagaglio;
+    private String codicePosto; // esempio 1-4 Colonna , 1-10 Riga (1-4)
 
     //Metodi get
     public String getNomePasseggero(){return nomePasseggero;}
     public String getCognomePasseggero(){return cognomePasseggero;}
     public int getGate(){return gate;}
     public LocalDate getOrario(){return orario;}
-    public String getArrivo(){return arrivo;}
+    public String getPartenza(){return partenza;}
     public String getDestinazione(){return destinazione;}
     public String getIdRiconoscimentoBagaglio(){return idRiconoscimentoBagaglio;}
 }
