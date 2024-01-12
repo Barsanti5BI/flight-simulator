1ae5ab05b3c01f2035a3c7e8d9f0ba1921748ed2
jessyjackson <104509645+jessyjackson@users.noreply.github.com>
2024-01-12 08:56:42 +0100

    Edit Files

--- src/Aereoporto/ZonaCheckIn/NastroTrasportatore.java
+++ src/Aereoporto/ZonaCheckIn/NastroTrasportatore.java
@@ -1,6 +1,16 @@
 package Aereoporto.ZonaCheckIn;
 
+import Persona.Bagaglio;
+import Utils.Coda;
+
 public class NastroTrasportatore {
    //Lista di bagagli che sono stati caricati sul nastro trasportatore iseriti a indice in base alla posizione
    //del banco di check-in da cui sono stati caricati
+   Coda<Bagaglio> bagagliCaricati;
+   public void  CaricaBagaglio(Bagaglio b){
+        bagagliCaricati.push(b);
+   }
+    public Bagaglio ScaricaBagaglio(){
+         return bagagliCaricati.pop();
+    }
 }
