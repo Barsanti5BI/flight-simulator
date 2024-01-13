46da4d97addba1144b3c015e9bafa452a49e732a
AlessioCampagnaro <alessio.campagnaro@barsanti.edu.it>
2024-01-13 09:44:49 +0100

    jfb

--- src/Aereo/AereoPasseggeri.java
+++ src/Aereo/AereoPasseggeri.java
@@ -5,29 +5,32 @@ import Utils.Coda;
 import java.util.ArrayList;
 
 
-public class AereoPasseggeri extends Aereo{
+public class AereoPasseggeri extends Aereo {
 
 
     private Turista[][] matricePostiAereo;
     private int nPosti;
     private Entrata entrata;
 
+    private Uscita uscita;
 
-    public AereoPasseggeri(String nome,ArrayList<Pilota> piloti){
+
+    public AereoPasseggeri(String nome, ArrayList<Pilota> piloti) {
         super(nome, piloti);
         matricePostiAereo = new Turista[4][10];
         nPosti = 40;
         entrata = new Entrata();
+        uscita = new Uscita(this);
 
     }
 
-    public void ImbarcaPasseggieri(){
+    public void ImbarcaPasseggieri() {
         Imbarca(entrata.GetsalitiDavanti);
         Imbarca(entrata.GetsalitiDavanti);
     }
 
-    public void Imbarca(Coda<Turista> c){
-        for (int i=0;i<c.size();i++){
+    public void Imbarca(Coda<Turista> c) {
+        for (int i = 0; i < c.size(); i++) {
             Turista t = c.pop();
             matricePostiAereo[t.colonna][t.riga] = t;
 
@@ -35,12 +38,12 @@ public class AereoPasseggeri extends Aereo{
 
     }
 
-    public Coda<Turista> FaiScendere(){
+    public Coda<Turista> FaiScendere() {
 
         Coda<Turista> coda = new Coda<Turista>();
-        for(int c = 0;c<4;c++){
-            for(int r=0;r<10;r++){
-                if( matricePostiAereo[c][r] != null){
+        for (int c = 0; c < 4; c++) {
+            for (int r = 0; r < 10; r++) {
+                if (matricePostiAereo[c][r] != null) {
                     coda.push(matricePostiAereo[c][r]);
                     matricePostiAereo[c][r] = null;
                 }
@@ -48,6 +51,14 @@ public class AereoPasseggeri extends Aereo{
             }
         }
         return coda;
+    }
+
+    public Entrata getEntrata(){
+        return entrata;
+    }
+
+    public Uscita getUscita(){
+        return uscita;
     }
 }
     
