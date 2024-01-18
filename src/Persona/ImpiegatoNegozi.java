de69314eb597e92339129e48845336a686ea00d8
willyyyyyy0000333 <squizz03@gmail.com>
2024-01-18 18:11:00 +0100

    Modifica classe ImpiegatoNegozi al metodo Vendi()

--- src/Persona/ImpiegatoNegozi.java
+++ src/Persona/ImpiegatoNegozi.java
@@ -1,7 +1,5 @@
 package Persona;
-
 import Utils.Coda;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -21,77 +19,27 @@ public class ImpiegatoNegozi extends Persona{
         this.turisti = turisti;
     }
 
-    // metodo sbagliato, perchè bisogna far gestire la scelta al cliente
-    // l'impiegatoNegozi devi occuparsi solo della cassa
-    // quando il cliente avrà bisogna mettere la variabile boolean pagato su true
     public void run(){
-        //Aggiungere sconto se si supera un determinato importo
-//        System.out.println("Nuovo cliente entrato nel negozio");
-//        int numeroProdottiAcquistati = new Random().nextInt(19) + 1;
-//        for (int i = 0; i < numeroProdottiAcquistati; i++) {
-//            List<Prodotto> prodottiDisponibili = getProdottiDisponibili();
-//            if (!prodottiDisponibili.isEmpty()) {
-//                Prodotto prodottoScelto = prodottiDisponibili.get(new Random().nextInt(prodottiDisponibili.size()));
-//                System.out.println("Prodotto/i venduto/i: " + prodottoScelto.getNome());
-//                System.out.println("Prezzo del prodotto: " + prodottoScelto.getPrezzo());
-//                importo = prezzo.getPrezzo() + prodottoScelto.getPrezzo();
-//                System.out.println("Importo parziale della vendita: " + importo);
-//            }
-//        }
-//        System.out.println("Importo totale della vendita: " + importo);
         Turista turista = turisti.pop();
-        // mettere in questa parte il codice da sistemare
-
         synchronized (turista)
         {
             turista.notify();
         }
     }
-
-    // bisogna farla gestire all'aeroporto
-    private List<Prodotto> getProdottiDisponibili() {
-        List<Prodotto> prodotti = new ArrayList<Prodotto>();
-        prodotti.add(new Prodotto("Calzini", 10.0));
-        prodotti.add(new Prodotto("Tazza", 20.0));
-        prodotti.add(new Prodotto("Sacchetto cioccolatini", 15.0));
-        prodotti.add(new Prodotto("Bottiglia alcol", 25.0));
-        prodotti.add(new Prodotto("T-Shirt", 30.0));
-        prodotti.add(new Prodotto("Cuscino per aereo", 35.0));
-        prodotti.add(new Prodotto("Coperta", 40.0));
-        prodotti.add(new Prodotto("Camicia", 45.0));
-        prodotti.add(new Prodotto("Felpa", 50.0));
-        prodotti.add(new Prodotto("Set calamite", 55.0));
-        prodotti.add(new Prodotto("Quadro", 60.0));
-        prodotti.add(new Prodotto("Auricolari", 65.0));
-        prodotti.add(new Prodotto("Adattatore", 70.0));
-        prodotti.add(new Prodotto("Mouse", 75.0));
-        prodotti.add(new Prodotto("Cuffie", 80.0));
-        prodotti.add(new Prodotto("Giocattolo", 85.0));
-        prodotti.add(new Prodotto("Scarpe", 90.0));
-        prodotti.add(new Prodotto("Lego", 95.0));
-        prodotti.add(new Prodotto("Occhiali da sole", 100.0));
-        return prodotti;
-    }
-
-    public void Vendi() {
+    public void Vendi(Turista turista) {
         double sconto;
-        System.out.println("Nuovo cliente entrato nel negozio: " + nome);
-        if (prodottoInVendita != null && !prodottoInVendita.isEmpty()) {
-            for (Prodotto prodotto : prodottoInVendita) {
-                System.out.println("Prodotto venduto: " + prodotto.getNome() + " a prezzo: " + prodotto.getPrezzo());
-                importo += prodotto.getPrezzo();
-            }
-            System.out.println("Importo totale della vendita: " + importo);
+        // Calcola l'importo basato sugli oggetti selezionati dal turista
+        for (Prodotto prodotto : turista.oggettiDaComprare) {
+            System.out.println("Prodotto venduto: " + prodotto.getNome() + " a prezzo: " + prodotto.getPrezzo());
+            importo += prodotto.getPrezzo();
+        }
+        System.out.println("Importo totale della vendita: " + importo);
             // Sconto 20% se l'importo supera i 100 euro
-            if (importo > 100.0) {
-                sconto = importo * 0.20;
-                importo -= sconto;
-                System.out.println("Sconto applicato: " + sconto);
-            }
-            System.out.println("Importo totale della vendita dopo lo sconto: " + importo);
+        if (importo > 100.0) {
+            sconto = importo * 0.20;
+            importo -= sconto;
+            System.out.println("Sconto applicato: " + sconto);
         }
-        else {
-            System.out.println("Nessun prodotto in vendita o lista prodotti non disponibile.");
-        }
+        System.out.println("Importo totale della vendita dopo lo sconto: " + importo);
     }
 }
