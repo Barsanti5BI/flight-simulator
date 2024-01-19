20bcec1e3c0687e698cd43aa6a7c8620d4afa08c
RPette <118899569+RPette@users.noreply.github.com>
2024-01-19 19:16:53 +0100

    Add Main

--- src/Main.java
+++ src/Main.java
@@ -8,19 +8,18 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
+import Aereo.AereoPasseggeri;
+
 public class Main {
    public static void main(String[] args) {
-      System.out.println("Airport Simulation");
 
       LinkedList<Aereo> lista_aerei = new LinkedList<Aereo>();
 
       for (int i = 0; i <= 9; i++){
-         lista_aerei.add(new Aereo(i) {
-         });
+         AereoPasseggeri a = new AereoPasseggeri(i);
+         lista_aerei.add((Aereo) a);
       }
 
-      ImpiegatoControlliStiva giano =  new ImpiegatoControlliStiva(new NastroTrasportatore(), lista_aerei);
-
-      // TODO: generare turisti e farli entrare in aeroporto
+      ImpiegatoControlliStiva impiegatoControlliStiva =  new ImpiegatoControlliStiva(new NastroTrasportatore(), lista_aerei);
    }
 }
