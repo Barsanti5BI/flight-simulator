83ab8b67b610dcf1623bbd36471e3a709cf79bb3
RPette <118899569+RPette@users.noreply.github.com>
2024-01-19 22:15:18 +0100

    Last Upgrade (I hope)

--- src/Aereoporto/ZonaCheckIn/Banco.java
+++ src/Aereoporto/ZonaCheckIn/Banco.java
@@ -30,10 +30,10 @@ public class Banco {
 
     public CartaImbarco generaCartaImbarco(Turista p,Viaggio viaggio){
         // aggiungere i dati sui voli
-        return new CartaImbarco(p.getDoc().getNome(), p.getDoc().getCognome(), viaggio.GetNumGate(), LocalDate.now(), viaggio.Get_Aereo().getName(), p.getBagaglio().getEtichetta().getIdRiconoscimentoBagaglio(), Math.random() > 0.5);
+        return new CartaImbarco(p.getDoc().getNome(), p.getDoc().getCognome(), viaggio.GetNumGate(), LocalDate.now(), viaggio.getAereo().getName(), p.getBagaglio().getEtichetta().getIdRiconoscimentoBagaglio(), Math.random() > 0.5);
     }
     public Etichetta generaEtichetta(Turista t,Viaggio viaggio){
-        return new Etichetta(viaggio.Get_Aereo().Get_ID(),t.getBagaglio().getEtichetta().getIdRiconoscimentoBagaglio());
+        return new Etichetta(viaggio.getAereo().Get_ID(),t.getBagaglio().getEtichetta().getIdRiconoscimentoBagaglio());
     }
     public Coda<Turista> getCodaTuristi() {
         return codaTuristi;
