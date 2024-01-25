0a118a06df28bf57c342534332d9025b60ba9f99
AlessioCampagnaro <alessio.campagnaro@barsanti.edu.it>
2024-01-25 17:42:30 +0100

    torre ctrl

--- src/Aereo/F_TorreControllo.java
+++ src/Aereo/F_TorreControllo.java
@@ -4,8 +4,9 @@ import javax.management.DynamicMBean;
 import java.util.Dictionary;
 import java.util.HashMap;
 
-public class F_TorreControllo {
+public class F_TorreControllo extends Thread{
 
+    private String nomeAerereoporto;
     private  Coda<Aereo> AereiInPartenza;
     private  Coda<Aereo> AereiInArrivo;
 
@@ -13,14 +14,25 @@ public class F_TorreControllo {
 
     private Dictionary<Aereo,String> viaggi;
 
-    public F_TorreControllo(Dictionary<String,F_TorreControllo> Aereoporti, Dictionary<Aereo,String> v){
-        this.Aereoporti =Aereoporti;
-        this.viaggi = v;
+    public F_TorreControllo(String nome){
+       nomeAerereoporto= nome;
     }
+
+    public void setAereoporti(Dictionary<String,F_TorreControllo> Ap){
+        Aereoporti=Ap;
+    }
+    public void AddViaggio(Dictionary<Aereo,String> v){
+        viaggi = v;
+    }
+
 
     public String GetDestinazione(Aereo a) {
         return viaggi.get(a);
     }
+
+
+
+
 
 
 
