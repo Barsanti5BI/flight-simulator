449d5752d8ca8ff339ec31cc0a4c00a8b990baee
AlessioCampagnaro <alessio.campagnaro@barsanti.edu.it>
2024-01-25 16:52:12 +0100

    Aereo Unico

--- src/Aereo/Aereo.java
+++ src/Aereo/Aereo.java
@@ -1,5 +1,7 @@
 package Aereo;
 
+import Utils.Coda;
+
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -18,6 +20,11 @@ public abstract class Aereo extends  Thread{
     public boolean einvolo;
     private boolean maltempo;
     private Random r;
+
+    private F_Turista[][] matricePostiAereo;
+    private int nPosti;
+    private Entrata entrata;
+    private Uscita uscita;
 
     public Aereo(int Id){
         this.id =Id;
@@ -40,6 +47,11 @@ public abstract class Aereo extends  Thread{
 
         alieni = new Alieni(this);
         alieni.start();
+
+        matricePostiAereo = new F_Turista[4][10];
+        nPosti = 40;
+        entrata = new Entrata();
+        uscita = new Uscita(this);
     }
 
     public void run(){
@@ -71,6 +83,7 @@ public abstract class Aereo extends  Thread{
 
     //Metodo che accende le Turbine dell'aereo e accende la scatola nera
     public void avvia(){
+        ImbarcaPasseggieri();
         for(int i = 0; i< 4;i++){
             turbine.get(i).start();
         }
@@ -152,6 +165,46 @@ public abstract class Aereo extends  Thread{
     public Serbatoio Get_Serbatoio(){
         return this.serbatoio;
     }
+
+    public void ImbarcaPasseggieri() {
+        Imbarca(entrata.GetsalitiDavanti());
+        Imbarca(entrata.GetsalitiDietro());
+    }
+
+    public void Imbarca(Coda<F_Turista> c) {
+
+        for (int i = 0; i < c.size(); i++) {
+            F_Turista t = c.pop();
+
+
+            matricePostiAereo[t.posto_colonna][t.posto_riga] = t;
+
+        }
+    }
+
+    public Coda<F_Turista> FaiScendere() {
+
+        Coda<F_Turista> coda = new Coda<F_Turista>();
+        for (int c = 0; c < 4; c++) {
+            for (int r = 0; r < 10; r++) {
+                if (matricePostiAereo[c][r] != null) {
+                    coda.push(matricePostiAereo[c][r]);
+                    matricePostiAereo[c][r] = null;
+                }
+
+            }
+        }
+        return coda;
+    }
+
+    public Entrata getEntrata(){
+        return entrata;
+    }
+
+    public Uscita getUscita(){
+        return uscita;
+    }
+
 }
 
 
